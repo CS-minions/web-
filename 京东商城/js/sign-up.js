cc7bebbf80a5efc8f81da3c6a9f0258d41599aba
CS-minions <17330004bc@gmail.com>
2024-11-27 17:27:14 +0800

    Update sign-up.html and sign-up.js for phone number validation and improved user feedback

--- 京东商城/js/sign-up.js
+++ 京东商城/js/sign-up.js
@@ -1,103 +1,56 @@
-// $('footer').hide();
-$('.address div').children('.A').hide();
-// $('.address div').children('.usually').hide();
-$('.tip1').hide();
-$('.tip2').hide();
-$('.tip3').hide();
-$('.tip4').hide();
-$('.tip-right').hide();
-$('.address>div').hide();
-$('.many').hover(function(){
-	$('.address').css('border-right','1px solid #000');
-	$('.many').css('border','1px solid #000');
-	$('.many').children().click(function(){
-		$('.address').css('border-right','1px solid #dddddd');
-		$('.many').css('border','1px solid #dddddd');
-		$(this).siblings().removeClass('border');
-		$(this).addClass('border');
-		if($('.address').hasClass('border')){
-			$('.address').css('border-right','1px solid #000');
-		}
-		return false;
-	})
-},function(){
-	if($('.address').hasClass('border')){
-		$('.address').css('border-right','1px solid #000');
-		$('.many').css('border','1px solid #dddddd');
-	}else{
-		$('.address').css('border-right','1px solid #dddddd');
-		$('.many').css('border','1px solid #dddddd');
-	}	
-	
-})
+$(document).ready(function() {
+    // 手机号正则表达式
+    var deg = /^[0-9]{11}$/;
 
-$('html').click(function(){
-	$('.many').children().removeClass('border');
-	$('.address').css('border-right','1px solid #dddddd');
-	$('.many').css('border','1px solid #dddddd');
-})
-$('.address p').eq(0).children().click(function(){
-	$(this).siblings().removeClass('red');
-	$(this).addClass('red');
-	var index=$(this).index();
-	var num=$(this).text();
-	$('.address p').eq(1).text(num);
-	$('.address ol').eq(index).siblings('ol').hide();
-	$('.address ol').eq(index).show();
-});
+    // 点击验证按钮
+    $('#verify-phone').click(function() {
+        var phone = $('#phone').val().trim();  // 获取手机号
+        if (phone === '') {
+            $('.tip1').show();  // 显示请输入手机号提示
+            $('.tip3').hide();  // 隐藏手机号格式错误提示
+            $('.tip-right').hide();  // 隐藏手机号格式正确提示
+            return false;
+        } else if (!phone.match(deg)) {
+            $('.tip3').show();  // 显示手机号格式错误提示
+            $('.tip-right').hide();  // 隐藏手机号格式正确提示
+            return false;
+        }
 
-var arr=['a.png','a.png','b.png','c.png','d.png','e.png','f.png','g.png','h.png','j.png'];
-var arr1=['b.png','1.png','2.png','3.png','4.png','5.png','6.png','7.png','8.png','9.png']
-for(var i=0;i<$(arr).length;i++){
-	$('.usually>li').eq(i).children('span').eq(0).css('background','url(../images/flag/'+arr[i]+')');
-	$('.usually>li').eq(i).children('span').eq(0).css('background-size','22px 16px');
-}
-for(var i=0;i<$(arr1).length;i++){
-	$('.A>li').eq(i).children('span').eq(0).css('background','url(../images/flag/'+arr1[i]+')');
-	$('.A>li').eq(i).children('span').eq(0).css('background-size','22px 16px');
-}
-$('.address ol>li').click(function(){
-	// $('.address>.first').text($(this).text());
-		$('.address>.first').text($(this).children().eq(1).text());
-		$('.address>.second').text($(this).children().eq(2).text());
-})
-$('.address').click(function(){
-	$('.address>div').show();	
-})
-$('html').click(function(){
-	$('.address>div').hide();	
-});
-//输入框
-$('.many>input').click(function(){
-	$('.address>div').hide();	
-});
+        // 验证通过后，显示验证码输入框
+        $('#verification-code').show();
+        $('#verify-code').show();
+        $('.tip-right').show();  // 显示手机号格式正确提示
+        alert('手机号验证成功！请输入验证码');
+        return false;
+    });
 
-$('.many>input').focus(function(){
-	$('.tip1').show();
-	$('.tip1').siblings('.tip').hide();
-})
+    // 验证验证码
+    $('#verify-code').click(function() {
+        var verificationCode = $('#verification-code').val().trim();
+        if (verificationCode === '') {
+            alert('请输入验证码');
+            return false;
+        }
 
+        // 验证通过后，跳转到下一步
+        window.location.href = 'sign-up-two.html';
+    });
 
-//手机号
-var deg=/^[0-9]{11}$/;
-$('.many>input').blur(function(){
-	$('.tip1').hide();
-	if($('.many>input').val()!=''){
-		if(!$('.many>input').val().match(deg)){
-			$('.tip3').show();
-		}else{
-			$('.tip-right').show();
-		}		
-	}
-})
-$('button').click(function(){
-	var flag=true;
-	var d=$('.many>input').val();
-	if(d==''){
-		$('.tip2').show();
-		flag=false;
-	}else if(!$('.many>input').val().match(deg)){
-		flag=false;
-	}
-	return flag;
+    // 表单提交前的验证
+    $('form').on('submit', function(e) {
+        e.preventDefault();  // 阻止默认提交
+        var phone = $('#phone').val().trim();
+        if (phone === '') {
+            $('.tip1').show();  // 显示请输入手机号提示
+            return false;
+        } else if (!phone.match(deg)) {
+            $('.tip3').show();  // 显示手机号格式错误提示
+            return false;
+        }
+
+        // 其他表单验证通过后，进行表单提交
+        // 可根据实际需求修改
+        alert('手机号验证成功，提交表单');
+        this.submit();  // 提交表单
+    });
 });
