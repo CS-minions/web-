583f8e7c7271365d3c74c4d21e9a11ebfbe2f04a
“CS-minions“ <17330004bc@gmail.com>
2024-11-27 20:16:00 +0800

    Add new document and image files for course materials

--- 京东商城/js/index.js
+++ 京东商城/js/index.js
@@ -389,12 +389,12 @@ $('.fix3 li').eq(1).hover(function () { // 当鼠标悬停在 .fix3 下的第二
 	$(this).css('background-size', '56px 56px'); // 设置背景大小为 56px x 56px。
 });
 
-$('.fix3 li').eq(7).hide(); // 隐藏 .fix3 下的第八个 li 元素。
+$('.fix3 li').eq(4).hide(); // 隐藏 .fix3 下的第八个 li 元素。
 $(window).scroll(function (event) { // 绑定滚动事件处理函数。
 	if ($(window).scrollTop() >= 1200) { // 如果滚动距离大于等于 1200 像素。
-		$('.fix3 li').eq(7).slideDown(); // 下拉显示 .fix3 下的第八个 li 元素。
+		$('.fix3 li').eq(4).slideDown(); // 下拉显示 .fix3 下的第八个 li 元素。
 	} else {
-		$('.fix3 li').eq(7).slideUp(); // 向上收起隐藏 .fix3 下的第八个 li 元素。
+		$('.fix3 li').eq(4).slideUp(); // 向上收起隐藏 .fix3 下的第八个 li 元素。
 	}
 });
 
